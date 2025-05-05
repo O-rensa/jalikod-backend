2a1dbed240839aa3dd5af16028234d754b6e629a
Lorenz Lendio <131181504+O-rensa@users.noreply.github.com>
2025-05-06 00:05:38 +0800

    Added setters and getters

--- bffe/internal/domain/aggregates/user.go
+++ bffe/internal/domain/aggregates/user.go
@@ -1,18 +1,60 @@
 package domainaggregates
 
 import (
-	"github.com/google/uuid"
+	"strings"
+
 	domainentities "github.com/o-rensa/jalikod-backend/bffe/internal/domain/entities"
 )
 
 type User struct {
-	ID            int32
-	FirstName     string
-	MiddleInitial *string
-	Surname       string
-	NameExtension *string
-	Username      string
-	Password      *string
-	SecurityStamp uuid.NullUUID
+	id            int32
+	firstName     string
+	middleInitial *string
+	surname       string
+	nameExtension *string
+	username      string
 	domainentities.FullAuditedEntity
+}
+
+func NewUser(id int32, username string) *User {
+	u := &User{}
+	u.id = id
+	u.username = username
+	return u
+}
+
+// setters
+func (u *User) SetFullName(firstname string, middle string, surname string, extension string) {
+	u.firstName = firstname
+	u.surname = surname
+	if strings.TrimSpace(middle) == "" {
+		u.middleInitial = nil
+	} else {
+		m := middle
+		u.middleInitial = &m
+	}
+
+	if strings.TrimSpace(extension) == "" {
+		u.nameExtension = nil
+	} else {
+		e := extension
+		u.nameExtension = &e
+	}
+}
+
+// getters
+func (u *User) GetId() int32 {
+	return u.id
+}
+
+func (u *User) GetFullname() (firstname string, middleInitial *string, surname string, nameExtension *string) {
+	firstname = u.firstName
+	middleInitial = u.middleInitial
+	surname = u.surname
+	nameExtension = u.nameExtension
+	return
+}
+
+func (u *User) GetUsername() string {
+	return u.username
 }
